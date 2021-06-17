de6b26e7a1a8644564294045b3373c4bc82650c2
Amdadur <amdad@live.co.uk>
2021-06-17 01:24:07 +0100

    submission

--- clipboard.js
+++ clipboard.js
@@ -1,3 +1,5 @@
+//const buttons = document.querySelectorAll(".play"); 
+
 function getWinner(playerMove, computerMove) {
     if (playerMove === computerMove){
         games++;
@@ -30,3 +32,53 @@ function getWinner(playerMove, computerMove) {
     else {
         return "catastrophic failure";}
 }
+
+
+//do {
+//var input = prompt("make your choice", "rock, paper or scissor");
+//if (input != null && ((input==="rock")||(input==="paper")||(input==="scissor"))) {
+//    let number = Math.floor(Math.random() * 3);
+//    let move = moves[number];
+//    let result = getWinner(input, move);
+//    alert(result);
+//    continueGame = confirm("do you want to play again?");
+//}
+//} while (continueGame)
+
+
+if (input != null && ((input==="rock")||(input==="paper")||(input==="scissor"))
+
+//for (let i = 0; i < resetButton.length; i++) {
+//    resetButton[i].addEventListener("on click", resetGame);}
+
+//resetButton.addEventListener('click', function(){
+//    resetGame();
+//})
+
+//event listener for buttons - (on click, start playGame function)
+//for (let i = 0; i < buttons.length; i++) {
+//    buttons[i].addEventListener("click", playGame);}
+
+
+//function resetGame() {
+//    result = "#";
+//    games = 0;
+//    winCount = 0;
+//    loseCount = 0;
+//    drawCount = 0;
+//    winStreak = 0;
+//
+//    showResult.innerHTML = "Result " + result;
+//    totalPlayed.innerHTML = "Round " + games;
+//    totalWins.innerHTML = "Wins " + winCount;
+//    totalLosses.innerHTML = "Losses " + loseCount;
+//    totalDrawn.innerHTML = "Drawn " + drawCount;
+//    totalWinStreak.innerHTML = "Wins in a row " + winStreak;
+//}
+
+            <!--<div class="item3">
+            <button class="playrock" type="button">rock</button>
+            <button class="play" type="button">paper</button>
+            <button class="play" type="button">scissor</button>
+            
+            </div>-->
--- main.js
+++ main.js
@@ -1,62 +1,152 @@
+//dom elements
+const rock_div = document.getElementById("rock");
+const paper_div = document.getElementById("paper");
+const scissor_div = document.getElementById("scissor");
+
+const showResult = document.querySelector(".result");
+const totalPlayed = document.querySelector(".rounds");
+const totalWins = document.querySelector(".wins");
+const totalLosses = document.querySelector(".losses");
+const totalDrawn = document.querySelector(".draws");
+const totalWinStreak = document.querySelector(".streak");
+
+const fxDiv = document.querySelector(".fx");
+
+//global variables
 let playerMove = "#";
 let computerMove = "#";
+let result = "#";
 var moves = ["rock", "paper", "scissor"];
 var games = 0;
 var winCount = 0;
 var loseCount = 0;
 var drawCount = 0;
-var continueGame = false; 
+let winStreak = 0;
 
-
-for ( let i = 0 ; i < buttons.length ; i++){
-buttons[i].addEventListener('click', playGame);
-
-
+//takes player choice and randomised computer move to work out outcome - logic
 function getWinner(playerMove, computerMove) {
-    if (playerMove === computerMove){
+    if (playerMove === computerMove) {
         games++;
         drawCount++;
-        return 0;}
+        result = "draw";
+        return 0;
+    }
 
-    if (playerMove === "rock"){
-        if (computerMove === "paper"){
-            games++;
-            loseCount++;
-            return -1; 
-        } else {
+    if (playerMove === "rock") {
+        if (computerMove === "scissor") {
             games++;
             winCount++;
-            return 1;}}
-
-    if (playerMove === "paper"){ 
-        if (computerMove === "rock"){
-            games++;
-            winCount++;
+            result = "win";
+            winStreak++;
             return 1;
         } else {
             games++;
             loseCount++;
-            return -1;}}
-    
-    if (playerMove === "scissor"){
-        if (computerMove === "rock"){
-            games++;
-            loseCount++;
-            return -1;}
-        } else {
+            result = "lose";
+            winStreak = 0;
+            return -1;
+        }
+    }
+
+    if (playerMove === "paper") {
+        if (computerMove === "rock") {
             games++;
             winCount++;
-            return 1;}}
+            result = "win";
+            winStreak++;
+            return 1;
+        } else {
+            games++;
+            loseCount++;
+            result = "lose";
+            winStreak = 0;
+            return -1;
+        }
+    }
 
-
-do {
-var input = prompt("make your choice", "rock, paper or scissor");
-if (input != null && ((input==="rock")||(input==="paper")||(input==="scissor"))) {
-    let number = Math.floor(Math.random() * 3);
-    let move = moves[number];
-    let result = getWinner(input, move);
-    alert(result);
-    continueGame = confirm("do you want to play again?");
+    if (playerMove === "scissor") {
+        if (computerMove === "paper") {
+            games++;
+            winCount++;
+            result = "win";
+            winStreak++;
+            return 1;
+        }
+    } else {
+        games++;
+        loseCount++;
+        result = "lose";
+        winStreak = 0;
+        return -1;
+    }
 }
 
-} while (continueGame);
+//event listeners for buttons - inputs player's choice
+rock_div.addEventListener("click", function () {
+    playGame("rock");
+});
+paper_div.addEventListener("click", function () {
+    playGame("paper");
+});
+scissor_div.addEventListener("click", function () {
+    playGame("scissor");
+});
+
+//global variables - forced outside playGame() as these are called by handAnimation()
+let input = "";
+let move = "";
+
+//takes input and randomises computer move. keeps count of wins, losses and times drawn
+function playGame(e) {
+    input = e;
+    let number = Math.floor(Math.random() * 3);
+    move = moves[number];
+    console.log(input + "&" + move);
+    getWinner(input, move);
+    handAnimation();
+    //used timeout to sync scoreboard with animation
+    setTimeout(() => {
+        showResult.innerHTML = result;
+        totalPlayed.innerHTML = "Round " + games;
+        totalWins.innerHTML = winCount;
+        totalLosses.innerHTML = loseCount;
+        totalDrawn.innerHTML = "Draws " + drawCount;
+        if (winStreak > 1) {
+            totalWinStreak.innerHTML =
+                "Win Streak &#128293;" + winStreak + "&#128293;";
+        } else if ((winStreak = 1)) {
+            totalWinStreak.innerHTML = "Win Streak " + winStreak;
+        } else {
+            totalWinStreak.innerHTML = "Win Streak";
+        }
+    }, 800);
+}
+
+//creates new elements temporarily - timeout and removals
+function handAnimation() {
+    var leftHand = document.createElement("img");
+    leftHand.setAttribute("id", "leftHand");
+    leftHand.src = `./images/${input}-small.png`;
+    fxDiv.appendChild(leftHand);
+    setTimeout(() => {
+        leftHand.remove();
+    }, 800);
+
+    var rightHand = document.createElement("img");
+    rightHand.setAttribute("id", "rightHand");
+    rightHand.src = `./images/${move}-small.png`;
+    fxDiv.appendChild(rightHand);
+    setTimeout(() => {
+        rightHand.remove();
+    }, 800);
+
+    setTimeout(() => {
+        var fxgif = document.createElement("img");
+        fxgif.setAttribute("id", "gif");
+        fxgif.src = "./images/fx.gif";
+        fxDiv.appendChild(fxgif);
+        setTimeout(() => {
+            fxgif.remove();
+        }, 430);
+    }, 800);
+}
